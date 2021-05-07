8918fe8891ddf8cf8f4f219d133fe4114cb568c5
Teah Markstone <teah.markstone@tufts.edu>
2021-05-07 12:54:43 -0400

    added cacti to desert scenes
    Currently do 1 damage if you run into them

---
+++ TowerSiegeGame/Assets/Scripts/Cacti.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cacti : MonoBehaviour
+{
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Apply damage and destroy the projectile if it collides with a unit or the player.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player>().TakeDamage(damage);
+        }
+        if (collision.gameObject.tag == "Unit")
+        {
+            collision.gameObject.GetComponent<Unit>().TakeDamage(damage);
+        }
+    }
+}
