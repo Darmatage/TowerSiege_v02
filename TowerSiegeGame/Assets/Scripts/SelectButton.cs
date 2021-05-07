4a121586912660c880cbb8c3030326e86017cfc0
spencerGallant <spencer.gallant@gmail.com>
2021-05-07 13:03:35 -0400

    selection bar now uses assets to determing numbers

--- TowerSiegeGame/Assets/Scripts/SelectButton.cs
+++ TowerSiegeGame/Assets/Scripts/SelectButton.cs
@@ -7,10 +7,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SelectButton : MonoBehaviour
 {
     public GameObject unit;
+
 
     private GameObject gameController;
     private GameObject startRoundButton;
@@ -20,6 +22,12 @@ public class SelectButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+    	print(unit.GetComponent<Unit>().health);
+    	this.transform.Find("CostText").gameObject.GetComponent<TextMeshProUGUI>().text = (unit.GetComponent<Unit>().cost).ToString();
+    	this.transform.Find("HealthText").gameObject.GetComponent<TextMeshProUGUI>().text = (unit.GetComponent<Unit>().health).ToString();
+    	this.transform.Find("SpeedText").gameObject.GetComponent<TextMeshProUGUI>().text = (unit.GetComponent<Unit>().speed).ToString();
+    	this.transform.Find("DamageText").gameObject.GetComponent<TextMeshProUGUI>().text = (unit.GetComponent<Unit>().damage).ToString();
+    	//print(unit.name);
         gameController = GameObject.FindGameObjectWithTag("GameController");
         startRoundButton = transform.parent.Find("StartRoundButton").gameObject;
         allButtons = transform.parent.GetComponentsInChildren<Image>();
