e4d63202881a104ac6d82aafa786f0bb9d02a4eb
colinrkeegan <71410202+colinrkeegan@users.noreply.github.com>
2021-05-10 17:38:40 -0400

    Started work on the tutorial

---
+++ TowerSiegeGame/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    public static bool isTutorial = false;
+    public GameObject scrollUI;
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+		scrollUI.SetActive(true);
+        Time.timeScale = 0f;
+        isTutorial = true;
+        player.GetComponent<Player>().Freeze();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTutorial) {
+            if (Input.anyKeyDown) {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        scrollUI.SetActive(false);
+        Time.timeScale = 1f;
+        isTutorial = false;
+        player.GetComponent<Player>().Unfreeze();
+    }
+
+}
